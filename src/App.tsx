22b8d3c1a0a9e5ed029390280c583ce1317daf85
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-17 06:28:05 +0900

    feature; 初期構築、supabaseAuth導入

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import type { Session } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import supabase from './utils/supabase'
+
+function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
+
+}
+
+export default App
