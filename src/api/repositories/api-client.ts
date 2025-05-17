bac8fddbb14d4f06cd13c48e1312237be593b082
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-17 23:06:47 +0900

    apiClientを追加

---
+++ src/api/repositories/api-client.ts
@@ -0,0 +1,25 @@
+import Axios, { InternalAxiosRequestConfig } from "axios";
+
+function authRequestInterceptor(config: InternalAxiosRequestConfig) {
+    if (config.headers) {
+        config.headers.Accept = 'application/json'
+    }
+
+    config.withCredentials = true
+    return config
+}
+
+export const api = Axios.create({
+    baseURL: import.meta.env.API_URL,
+})
+
+api.interceptors.request.use(authRequestInterceptor)
+api.interceptors.response.use(
+    (response) => {
+        return response.data
+    },
+
+    (error) => {
+        throw new Error(error);
+    }
+)
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
   readonly VITE_SUPABASE_URL: string;
   readonly VITE_SUPABASE_ANON_KEY: string;
+  readonly API_URL: string;
   // ...ここに追加していく
 }
 
