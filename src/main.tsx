628ed267bdfe22ca55a1f6b61ed74f096d913bf1
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-17 22:49:19 +0900

    error解消

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/App';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('No root element found');
 
 createRoot(root).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
