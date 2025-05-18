11d334c1113b679bd61e8da0210b6fbbd0f61cde
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-18 12:33:57 +0900

    feature; notification store

---
+++ src/components/ui/notifications/notification-store.ts
@@ -0,0 +1,50 @@
+import { atom, useAtom } from 'jotai'
+import { v4 as uuidv4 } from "uuid";
+
+export type Notification = {
+    id: string;
+    type: 'error';
+    message: string;
+};
+
+export type NotificationsStore = {
+    notifications: Notification[];
+    addNotification: (notification: Omit<Notification, 'id'>) => void;
+    dismissNotification: (id: string) => void;
+};
+
+const notificationsAtom = atom<Notification[]>([]);
+const addNotificationAtom = atom(
+    null,
+    (get, set, notification: Omit<Notification, 'id'>) => {
+      const id = uuidv4()
+      const newNotification: Notification = {
+          id,
+        ...notification,
+      };
+      
+      set(notificationsAtom, [...get(notificationsAtom), newNotification]);
+    }
+  );
+
+const dismissNotificationAtom = atom(
+    null,
+    (get, set, id: string) => {
+    set(
+        notificationsAtom,
+        get(notificationsAtom).filter(notification => notification.id !== id)
+    )
+    }
+)
+
+export function useNotificationStore(): NotificationsStore {
+    const [notifications] = useAtom(notificationsAtom)
+    const [, addNotification] = useAtom(addNotificationAtom)
+    const [, dismissNotification] = useAtom(dismissNotificationAtom)
+
+    return {
+        notifications,
+        addNotification,
+        dismissNotification,
+    }
+}
