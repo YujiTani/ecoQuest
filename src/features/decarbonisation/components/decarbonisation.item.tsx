1d08457dccfe8078002cd963dc94f2f78bea5d6c
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-19 09:32:10 +0900

    add; longPress handler

--- src/features/decarbonisation/components/decarbonisation.item.tsx
+++ src/features/decarbonisation/components/decarbonisation.item.tsx
@@ -1,7 +1,10 @@
+import { useLongPress } from "@/hooks/useLongClick";
 import { IconNames, ICONS } from "../constants";
+import { useRef } from "react";
 
 export type Decarbonisation = {
   id: number;
+  uuid: string;
   name: string;
   description: string;
   icon: IconNames;
@@ -16,12 +19,27 @@ type DecarbonisationProps = {
  * 脱炭素アクションアイテム
  */
 function DecarbonisationItem({ item }: DecarbonisationProps) {
+  const itemRef = useRef<HTMLDivElement>(null)
   const icon = ICONS[item.icon] || ICONS.ToyBrick;
+
+  function hancleLongPress() {
+    const itemElement = itemRef.current
+
+    if (itemElement) {
+      const uuid = itemElement.getAttribute("data-id")
+      console.log('uuid:', uuid)
+    }
+  }
+
+  const handleLongPress = useLongPress(hancleLongPress, 700)
 
   return (
     <div
+      ref={itemRef}
       key={item.id}
       className="overflow-hidden rounded-lg border-8 border-gray-200 bg-white shadow-sm transition-shadow duration-200 hover:shadow-md"
+      {...handleLongPress}
+      data-id={item.uuid}
     >
       <div className="flex h-60 items-center justify-center bg-gray-100">
         <img className="object-cover aspect-square w-full h-full" src={item.image} alt={item.name} />
