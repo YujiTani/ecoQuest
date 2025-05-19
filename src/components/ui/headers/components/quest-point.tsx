20a972db8d9425db9407014983d33bb5052337f6
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-19 09:40:37 +0900

    fix; textサイズ

--- src/components/ui/headers/components/quest-point.tsx
+++ src/components/ui/headers/components/quest-point.tsx
@@ -10,14 +10,14 @@ function QuestPoint() {
   return (
     <div className="flex items-center space-x-6">
       <div>
-        <p className="text-xs text-gray-500">合計</p>
+        <p className="text-xs text-gray-500">トータル</p>
         <p className="text-4xl font-semibold text-gray-900">
           {totalQuestPoint.toLocaleString()} pt
         </p>
       </div>
 
       <div>
-        <p className="text-xs text-gray-500">今週</p>
+        <p className="text-xs text-gray-500">今日</p>
         <p className="text-4xl font-semibold text-green-600">
           {todayQuestPoint.toLocaleString()} pt
         </p>
