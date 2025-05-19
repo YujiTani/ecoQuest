eb92722209cdc8911df7e51d578d46ffc3f7d73c
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-19 11:05:25 +0900

    feature; 達成時にquest_pointsテーブルにinsertするapi追加

--- src/features/decarbonisation/components/decarbonisation.grid.tsx
+++ src/features/decarbonisation/components/decarbonisation.grid.tsx
@@ -1,17 +1,35 @@
 import DecarbonisationItem from './decarbonisation.item';
 import { useDecarbonisations } from '../api/useDecarbonisation';
+import { useInsertManyQuestPoint } from '../api/useQuestPoint';
 
 /**
  * 脱炭素アクショングリッド
  */
 function DecarbonisationGrid() {
   const { decarbonisations } = useDecarbonisations();
+  const insertQuestPointMutation = useInsertManyQuestPoint();
 
   // アクション達成時の処理
   const handleAchieve = (uuid: string) => {
     // ここでAPIを呼び出して達成を記録することもできます
     // 例: saveAchievement(uuid);
-    console.log(`apiリクエスト: ${uuid}`)
+    const response = insertQuestPointMutation.mutate([
+      {
+      user_id: "a808ba52-64d5-456f-9590-da27cbb945eb",
+      decarbonisation_id: uuid,
+    },
+    {
+      user_id: "a808ba52-64d5-456f-9590-da27cbb945eb",
+      decarbonisation_id: uuid,
+    },
+    {
+      user_id: "a808ba52-64d5-456f-9590-da27cbb945eb",
+      decarbonisation_id: uuid,
+    },
+  ])
+
+  // TODO: 成功時、失敗時に通知を表示する
+  console.log('response:', response)
   };
 
   return (
