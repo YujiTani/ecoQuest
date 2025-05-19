a39b344a51c572517bf88728bb8c287c1e2ccaa1
Yuji Tani <valleyyuji.webengineer@gmail.com>
2025-05-19 22:40:22 +0900

    fix; userStoreから情報取得に変更

--- src/components/ui/headers/components/user.tsx
+++ src/components/ui/headers/components/user.tsx
@@ -1,10 +1,10 @@
-import { useUser } from '@/features/auth/api/useAuth';
+import { useUserStore } from '@/features/auth/api/user.store';
 
 /**
  * header; ユーザー情報を表示
  */
 function User() {
-  const { user } = useUser();
+  const { user } = useUserStore();
 
   return <>{user && <p>{user.user_metadata.full_name}</p>}</>;
 }
